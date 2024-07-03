cea17a0bea44213fab21b75e88e07bdeb57d9562
Manthan1615 <143803529+Manthan2343@users.noreply.github.com>
2024-07-03 21:51:41 +0530

    Add files via upload

---
+++ Basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*int arrary[10];
+
+    cout<<"Value at 0th index:"<< arrary[0]<<endl;
+*/
+    int limit[10]={1,2,3};
+    int n=10;
+    for(int i=0;i<n;i++){                   // array should never be greater than or equal to if we start from i=0 as arrary i (n-1) always7
+
+        cout<<limit[i]<<endl;
+    }
+}
